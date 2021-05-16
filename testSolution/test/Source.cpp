0640d7444a966899089e971eefa1767af54f1bda
陈靖文 <1071817669@qq.com>
2021-05-16 22:51:17 +0800

    5月16日
    自学c++的第一天

--- testSolution/test/Source.cpp
+++ testSolution/test/Source.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 int main()
 {
-    cout << "Hello, world" << endl;
-    return 0;
+	cout << "Please input three numbers" << endl;
+	int h[3];
+	for (int i = 0; i < 3; i++)
+	{
+		cin >> h[i];
+	}
+	
+}
+int top_down(int a[])
+{
+	int len = sizeof(a) / sizeof(a[0]);
+	if (len == 1)
+		return a[0];
+	top_down()
 }
