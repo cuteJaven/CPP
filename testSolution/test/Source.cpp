4b7862e29729300dd4068ebb7f4c74a79a0e6cb3
陈靖文 <1071817669@qq.com>
2021-05-31 19:04:40 +0800

    构造函数

--- testSolution/test/Source.cpp
+++ testSolution/test/Source.cpp
@@ -4,15 +4,28 @@ using namespace std;
 class Person
 {
 public:
-	Person()
+	//无参构造函数
+	Person() {}
+	//有参构造函数
+	Person(int age)
 	{
-		cout << "这里是构造函数" << endl;
+		m_age = age;
 	}
-	~Person()
+	//拷贝构造函数，常用于游戏中构造分身
+	Person(const Person& p)
 	{
-		cout << "这里是析构函数" << endl;
+		m_age = p.m_age;
 	}
+private:
+	int m_age = 0;
 };
+//构造函数可以有多个(重载)，但只会有一个被调用
+void test()
+{
+	Person p1;
+	Person p2(10);
+	Person p3(p2);
+}
 int main()
 {
 	Person p1;
